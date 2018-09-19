513cee25e605d44ce61cfd3408ce6a51640e5e5e
이상훈 <lshpianist@naver.com>
2018-09-19 17:12:31 +0900

    Add files via upload

---
+++ hello/variable2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1, num2, num3;
+	//각 변수는 int형
+	//각 변수를 다른 자료형으로 선언할려면
+	//각각 따로 선언해야됨
+	num1 = 10;
+	num2 = 20;
+	num3 = 30;
+
+	printf("%d %d %d\n", num1, num2, num3);
+
+	return 0;
+
+}
+
+
+
+
